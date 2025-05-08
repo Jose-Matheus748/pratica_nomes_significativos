5f38172cd36438d99f911a141ec51b067bc0178d
lasanhatamoyo <nunesadryan40@gmail.com>
2025-05-07 22:21:27 -0300

    Corrigindo exercicio 03

--- 03-medium.js
+++ 03-medium.js
@@ -2,39 +2,39 @@
  * Exercício 3: Métodos com nomes não descritivos
  * Problema: Os métodos e propriedades abaixo têm nomes que não descrevem bem o que fazem.
  */
-class gerenciar_usuario {
-  constructor(bandoDeDados) {
+class Usuario {
+  constructor(bancoDeDados) {
     this.bancoDeDados = bancoDeDados;
   }
 
   procurarUsuario(email, senha) {
-    const usuario = this.bancoDeDados.buscar_Por_Email(email);
+    const usuario = this.bancoDeDados.buscarPorEmail(email);
     if (!usuario) {
       return { sucesso: false, mensagem: "Usuário não encontrado" };
     }
 
     // Verifica se a senha coincide com a do banco de dados
-    if (this.bDados.vf_Senha(usuario.id, senha)) {
+    if (this.bancoDeDados.verificandoSenha(usuario.id, senha)) {
       // Cria token de autenticação
-      const tokenDeAutenticacao = this.bancoDeDados.g_Token(usuario.id);
+      const tokenDeAutenticacao = this.bancoDeDados.getToken(usuario.id);
       return { sucesso: true, tokenDeAutenticacao };
     } else {
       return { sucesso: false, mensagem: "Senha incorreta" };
     }
   }
 
-  getID(id) {
+  getId(id) {
     // Busca usuários por ID, exclui dados sensíveis
-    const usuario = this.bancoDeDados.b_Dados(id);
+    const usuario = this.bancoDeDados.buscarDados(id);
     if (usuario) {
-      const { senha, pin, ...d_p } = usuario;
-      return d_p;
+      const { senha, pin, ...dadosPessoais } = usuario;
+      return dadosPessoais;
     }
     return null;
   }
 
   excluirUsuario(id) {
     // Remove usuário da base de dados
-    return this.bancoDeDados.ru(id);
+    return this.bancoDeDados.removerUsuario(id);
   }
 }
